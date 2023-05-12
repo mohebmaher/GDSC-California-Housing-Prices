9811b9c686eb20b12d470bf77da9ce7df1413acd
moheb_maher <dvm.moheb.maher@gmail.com>
2023-05-12 21:42:35 +0300

    Create compare_models.py

---
+++ recipes/compare_models.py
@@ -0,0 +1,44 @@
+def compare_models(X, y, scoring, cv=5, random_state=42, estimators={}, **kwargs):
+    """
+    Compares different estimators.
+    """
+
+    from datetime import datetime
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    from sklearn.model_selection import cross_val_score, KFold
+
+    estimators.update(kwargs)
+    results = {}
+
+    if isinstance(cv, int):
+        cv = KFold(
+            n_splits=5,
+            shuffle=True,
+            random_state=random_state
+        )
+
+    for name, estimator in estimators.items():
+        tic = datetime.now()
+        print(f"Validating {name}...")
+        cv_results = cross_val_score(
+            estimator,
+            X,
+            y,
+            cv=cv,
+            scoring=scoring
+        )
+        results[name] = cv_results
+        toc = datetime.now()
+        tic_toc = (toc - tic).seconds / 60
+        print(f"Validating {name} done in {tic_toc:.2f} minutes!\n")
+
+    summary = pd.DataFrame(results).describe().transpose()
+    print(summary[["min", "mean", "max"]].round(2))
+    print("\n")
+
+    plt.boxplot(results.values(), labels=results.keys())
+    plt.xticks(rotation=90)
+    plt.show()
+
+    return summary
