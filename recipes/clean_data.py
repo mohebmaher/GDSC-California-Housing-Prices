286fc4f42259fa937c7ec9070a715a19da065f71
moheb_maher <dvm.moheb.maher@gmail.com>
2023-05-15 08:10:48 +0300

    Update clean_data.py

--- recipes/clean_data.py
+++ recipes/clean_data.py
@@ -1,11 +1,15 @@
-def clean_data(data, drop_cols=None, masks=None, drop_na=False):
+def clean_data(data, drop_labs=None, drop_cols=None, drop_na=False, drop_dups=None, masks=None):
     """Cleans data."""
     data_copy = data.copy()
+    if drop_labs is not None:
+        data_copy = data_copy.drop(drop_labs, axis=0)
     if drop_cols is not None:
         data_copy = data_copy.drop(drop_cols, axis=1)
+    if drop_na is True:
+        data_copy.dropna(inplace=True)
+    if drop_dups is True:
+        data_copy.drop_duplicates(inplace=True)
     if masks is not None:
         for mask in masks:
             data_copy = data_copy.query(f"{mask}").copy()
-    if drop_na is True:
-        data_copy.dropna(inplace=True)
     return data_copy
